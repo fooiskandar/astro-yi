23c650c03f36dbdaa4461d6ebd22a01109d97540
fooiskandar <fooiskandar@gmail.com>
2025-04-28 22:50:19 +0700

    Update utils.ts

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,10 +1,11 @@
 import {en} from './en'
 import {zhCn} from './zhCn'
 import {cs} from './cs'
+import {id} from './id'
 import {zhHant} from './zhHant'
 import {config} from "../consts";
 const ui = {
-  en, 'zh-cn': zhCn, 'zh-Hant': zhHant, cs
+  en, 'zh-cn': zhCn, 'zh-Hant': zhHant, cs, id
 }
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: string) {
